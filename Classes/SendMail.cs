c7d6b952f9ac4b2992da236760aa3b78ad6a074f
Илюха-денс <cheglakova666@gmail.com>
2026-02-21 12:03:28 +0500

    Разработанный класс отправки сообщения на почту. Марков

---
+++ Classes/SendMail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Markov.Classes
+{
+    public class SendMail
+    {
+        /// <summary>
+        /// Функция отправки сообщения
+        /// </summary>
+        /// <param name="Message">Сообщение которое необходимо отправить</param>
+        /// <param name="To">Почта на которую отправляется сообщение</param>
+        public static void SendMessage(string Message, string To)
+        {
+            // Создаём SMTP клиент, в качестве хоста указываем яндекс
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                // Указываем порт по которому передаём сообщение
+                Port = 587,
+                // Указываем почту, с которой будет отправляться сообщение, и пароль от этой почты
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                // Включаем поддержку SSL
+                EnableSsl = true,
+            };
+
+            // Вызываем метод send, который отправляет письмо на указанный адрес
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
+
