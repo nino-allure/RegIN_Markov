cb8d46fb65e880a5787193b2147f43aa8d3db173
Илюха-денс <cheglakova666@gmail.com>
2026-02-21 12:50:39 +0500

    Разработанный класс User. ну почти (массивы) Марков

--- Classes/SendMail.cs
+++ Classes/SendMail.cs
@@ -10,25 +10,14 @@ namespace RegIN_Markov.Classes
 {
     public class SendMail
     {
-        /// <summary>
-        /// Функция отправки сообщения
-        /// </summary>
-        /// <param name="Message">Сообщение которое необходимо отправить</param>
-        /// <param name="To">Почта на которую отправляется сообщение</param>
         public static void SendMessage(string Message, string To)
         {
-            // Создаём SMTP клиент, в качестве хоста указываем яндекс
             var smtpClient = new SmtpClient("smtp.yandex.ru")
             {
-                // Указываем порт по которому передаём сообщение
                 Port = 587,
-                // Указываем почту, с которой будет отправляться сообщение, и пароль от этой почты
                 Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
-                // Включаем поддержку SSL
                 EnableSsl = true,
             };
-
-            // Вызываем метод send, который отправляет письмо на указанный адрес
             smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
         }
     }
