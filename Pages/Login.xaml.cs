76d8ef03a0cdadc6f0c5a52c6384e4e6aac06d8d
DikiYLexa <dubovan36@gmail.com>
2026-03-02 16:18:31 +0500

    рограммирование страницы Login. markov.

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RegIN_Markov.Classes;
 using RegIN_Markov.Elements;
 
 namespace RegIN_Markov.Pages
@@ -28,6 +29,9 @@ namespace RegIN_Markov.Pages
         public Login()
         {
             InitializeComponent();
+            MainWindow.mainWindow.UserLogin.HandlerCorrectLogin += CorrectLogin;
+            MainWindow.mainWindow.UserLogin.HandlerInCorrectLogin += InCorrectLogin;
+            Capture.HandlerCorrectCapture += CorrectCapture;
         }
         string OldLogin;
         int CountSetPassword = 2;
@@ -140,6 +144,102 @@ namespace RegIN_Markov.Pages
             Capture.IsEnabled = false;
             IsCapture = true;
         }
+
+        private void SetPassword(object sender, KeyEventsArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SetPassword();
+            }
+        }
+        public void SetPassword()
+        {
+            if (MainWindow.mainWindow.UserLogin.Password != string.Empty)
+            {
+                if (IsCapture)
+                {
+                    if (MainWindow.mainWindow.UserLogin.Password == TbPassword.Password)
+                    {
+                        MainWindow.mainWindow.OpenPage(new Confirmation(Confirmation.TypeConfirmation.Login));
+                    }
+                    else
+                    {
+                        if (CountSetPassword > 0)
+                        {
+                            SetNotiffication($"Password is incorrect, {CountSetPassword} attempts left", Brushes.Red);
+                            CountSetPassword--;
+                        }
+                        else
+                        {
+                            Thread TBlockAuthoriation = new Thread(BlockAuthoriation);
+                            TBlockAuthoriation.Start();
+                            SendMail.SendMessage("An attempt was made to log into your account.", MainWindow.mainWindow.UserLogin.Login);
+                        }
+                    }
+                }            
+            }
+            else
+            {
+                SetNotification($"Enter Capture", Brushes.Red);
+            }
+        }
+        public void BlockAuthorization()
+        {
+            DateTime StartBlock = DateTime.Now.AddMinutes(3);
+            Dispatcher.Invoke(() =>
+            {
+                TbLogin.IsEnabled = false;
+                TbPassword.IsEnabled = false;
+                Capture.IsEnabled = false;
+            });
+            for (int i = 0; i < 100; i++)
+            {
+                TimeSpan TimeIdle = StartBlock.Subtract(DateTime.Now);
+                string s_minutes = TimeIdle.Minutes.ToString();
+                if (TimeIdle.Minutes < 10)
+                    s_minutes = "0" + TimeIdle.Minutes;
+                string s_seconds = TimeIdle.Seconds.ToString();
+                if(TimeIdle.Seconds < 10)
+                    s_seconds = "0" + TimeIdle.Seconds;
+                Dispatcher.Invoke(() =>
+                    {
+                        SetNotification($"Reathoruization available in: {s_minutes}:{s_seconds}", Brushes.Red);
+                    });
+                Thread.Sleep(1000);
+            }
+            Dispatcher.Invoke(() =>
+            {
+                SetNotification("Hi, " + MainWindow.mainWindow.UserLogin.Name, Brushes.Black);
+                TbLogin.IsEnabled = true;
+                TbPassword.IsEnabled = true;
+                Capture.IsEnabled = true;
+                Capture.CreateCapture();
+                IsCapture = false;
+                CountSetPassword = 2;
+            });
+        }
+        private void SetLogin(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MainWindow.mainWindow.UserLogIn.GetUserLogin(TbLogin.Text);
+                if (TbPassword.Password.Length > 0)
+                    SetPassword();
+            }
+        }
+        private void SetLogin(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainWindow.UserLogIn.GetUserLogin(TbLogin.Text);
+            if (TbPassword.Password.Length > 0)
+                SetPassword();
+        }
+        public void SetNottification(string Message, SolidColorBrush _Color)
+        {
+            LName_User.Content = Message;
+            LNameUser.Foreground = _Color;
+        }
+        private void RecoveryPassword(object sender, MouseButtonEventsArgs e) => MainWindow.mainWindow.OpenPage(new Recovery());
+        private void OpenRegin(object sender, MouseButtonEventsArgs e) => MainWindow.mainWindow.OpenPage(new Regin());
     }
 }
 
