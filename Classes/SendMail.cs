a4b74f891a37180f51c448de2c893c19e7efb0d2
DikiYLexa <dubovan36@gmail.com>
2026-03-14 13:42:23 +0500

    вроде работает но на  марков

--- Classes/SendMail.cs
+++ Classes/SendMail.cs
@@ -15,10 +15,10 @@ namespace RegIN_Markov.Classes
             var smtpClient = new SmtpClient("smtp.yandex.ru")
             {
                 Port = 587,
-                Credentials = new NetworkCredential("landaxer@yandex.ru", "password"), // Исправлено: согласован отправитель
+                Credentials = new NetworkCredential("kxzui@yandex.ru", "osnjdeoxyppxjoqx"), // 
                 EnableSsl = true,
             };
-            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message); // Исправлено: согласован отправитель
+            smtpClient.Send("kxzui@yandex.ru", To, "", Message); // Исправлено: согласован отправитель
         }
     }
 }
--- Pages/Regin.xaml.cs
+++ Pages/Regin.xaml.cs
@@ -13,11 +13,17 @@ using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using Org.BouncyCastle.Bcpg;
-using Imaging = Aspose.Imaging;
+using AsposeImage = Aspose.Imaging.Image;
+using DrawingImage = System.Drawing.Image;
+using System.Drawing;
+using System.Drawing.Imaging;
+using WpfImage = System.Windows.Controls.Image; // Алиас для WPF Image контрола
+
 namespace RegIN_Markov.Pages
 {
     /// <summary>
@@ -30,11 +36,11 @@ namespace RegIN_Markov.Pages
             InitializeComponent();
             MainWindow.mainWindow.UserLogin.HandlerCorrectLogin += CorrectLogin;
             MainWindow.mainWindow.UserLogin.HandlerInCorrectLogin += InCorrectLogin;
-            FileDialogImage.Filter = "PNG (*.png)|*.png|JPG (*.jpg)|*.jpg"; // Исправлено
+            FileDialogImage.Filter = "PNG (*.png)|*.png|JPG (*.jpg)|*.jpg";
             FileDialogImage.RestoreDirectory = true;
             FileDialogImage.Title = "Choose a photo for you pfp";
-
         }
+
         OpenFileDialog FileDialogImage = new OpenFileDialog();
         bool BCorrectLogin = false;
         bool BCorrectPassword = false;
@@ -43,11 +49,13 @@ namespace RegIN_Markov.Pages
 
         private void CorrectLogin()
         {
-            SetNotification("Login already in use", Brushes.Red);
+            SetNotification("Login already in use", System.Windows.Media.Brushes.Red);
             BCorrectLogin = false;
         }
+
         private void InCorrectLogin() =>
-            SetNotification("", Brushes.Black);
+            SetNotification("", System.Windows.Media.Brushes.Black);
+
         private void SetLogin(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -56,7 +64,7 @@ namespace RegIN_Markov.Pages
             }
         }
 
-        private void SetLogin(object sender, System.Windows.RoutedEventArgs e) =>
+        private void SetLogin(object sender, RoutedEventArgs e) =>
             SetLogin();
 
         public void SetLogin()
@@ -67,19 +75,20 @@ namespace RegIN_Markov.Pages
 
             if (regex.IsMatch(TbLogin.Text) == true)
             {
-                SetNotification("", Brushes.Black);
-                MainWindow.mainWindow.UserLogin.GetUserLogin(TbLogin.Text); // Исправлено: UserLogIn -> UserLogin
+                SetNotification("", System.Windows.Media.Brushes.Black);
+                MainWindow.mainWindow.UserLogin.GetUserLogin(TbLogin.Text);
             }
             else
             {
-                SetNotification("Invalid login", Brushes.Red);
+                SetNotification("Invalid login", System.Windows.Media.Brushes.Red);
             }
 
-            OnRegin(); // Исправлено: было OnRegion()
+            OnRegin();
         }
+
         #region SetPassword
 
-        private void SetPassword(object sender, System.Windows.RoutedEventArgs e) =>
+        private void SetPassword(object sender, RoutedEventArgs e) =>
             SetPassword();
 
         private void SetPassword(object sender, KeyEventArgs e)
@@ -96,16 +105,16 @@ namespace RegIN_Markov.Pages
 
             if (regex.IsMatch(TbPassword.Password) == true)
             {
-                SetNotification("", Brushes.Black);
+                SetNotification("", System.Windows.Media.Brushes.Black);
 
                 if (TbConfirmPassword.Password.Length > 0)
                     ConfirmPassword(true);
 
-                OnRegin(); // Исправлено: было OnRegion()
+                OnRegin();
             }
             else
             {
-                SetNotification("Invalid password", Brushes.Red);
+                SetNotification("Invalid password", System.Windows.Media.Brushes.Red);
             }
         }
         #endregion
@@ -120,7 +129,7 @@ namespace RegIN_Markov.Pages
             }
         }
 
-        private void ConfirmPassword(object sender, System.Windows.RoutedEventArgs e) =>
+        private void ConfirmPassword(object sender, RoutedEventArgs e) =>
             ConfirmPassword();
 
         public void ConfirmPassword(bool Pass = false)
@@ -129,11 +138,11 @@ namespace RegIN_Markov.Pages
 
             if (TbConfirmPassword.Password != TbPassword.Password)
             {
-                SetNotification("Passwords do not match", Brushes.Red);
+                SetNotification("Passwords do not match", System.Windows.Media.Brushes.Red);
             }
             else
             {
-                SetNotification("", Brushes.Black);
+                SetNotification("", System.Windows.Media.Brushes.Black);
 
                 if (!Pass)
                 {
@@ -154,13 +163,16 @@ namespace RegIN_Markov.Pages
                 return;
             if (!BCorrectConfirmPassword)
                 return;
+
             MainWindow.mainWindow.UserLogin.Login = TbLogin.Text;
             MainWindow.mainWindow.UserLogin.Password = TbPassword.Password;
             MainWindow.mainWindow.UserLogin.Name = TbName.Text;
-            if (BSetImages)
-                MainWindow.mainWindow.UserLogin.Image = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\IUser.jpg"); // Исправлено
+
+            if (BSetImages && File.Exists(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "IUser.jpg")))
+                MainWindow.mainWindow.UserLogin.Image = File.ReadAllBytes(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "IUser.jpg"));
+
             MainWindow.mainWindow.UserLogin.DateUpdate = DateTime.Now;
-            MainWindow.mainWindow.UserLogin.DateCreate = DateTime.Now; // Исправлено: добавлено DateCreate
+            MainWindow.mainWindow.UserLogin.DateCreate = DateTime.Now;
             MainWindow.mainWindow.OpenPage(new Confirmation(Confirmation.TypeConfirmation.Regin));
         }
 
@@ -168,68 +180,118 @@ namespace RegIN_Markov.Pages
         {
             e.Handled = !(Char.IsLetter(e.Text, 0));
         }
+
         public void SetNotification(string Message, SolidColorBrush _Color)
         {
             LNameUser.Content = Message;
             LNameUser.Foreground = _Color;
         }
+
         private void SelectImage(object sender, MouseButtonEventArgs e)
         {
             if (FileDialogImage.ShowDialog() == true)
             {
-                using (Imaging.Image image = Imaging.Image.Load(FileDialogImage.FileName))
+                try
                 {
-                    int NewWidth = 0;
-                    int NewHeight = 0;
+                    string outputPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "IUser.jpg");
 
-                    if (image.Width > image.Height)
+                    // Используем алиас для Aspose.Imaging.Image
+                    using (AsposeImage image = AsposeImage.Load(FileDialogImage.FileName))
                     {
-                        NewWidth = (int)(image.Width * (256f / image.Height));
-                        NewHeight = 256;
-                    }
-                    else
-                    {
-                        NewWidth = 256;
-                        NewHeight = (int)(image.Height * (256f / image.Width));
-                    }
-                    }
+                        int NewWidth = 0;
+                        int NewHeight = 0;
 
-                    using (System bitmap = new System.Drawing.Bitmap(image, NewWidth, NewHeight))
-                    {
-                        int X = 0;
-                        int Y = 0;
-                        if (bitmap.Width > 256)
-                            X = (bitmap.Width - 256) / 2;
-                        if (bitmap.Height > 256)
-                            Y = (bitmap.Height - 256) / 2;
-
-                        System.Drawing.Rectangle cropRect = new System.Drawing.Rectangle(X, Y, 256, 256);
-                        using (System.Drawing.Bitmap croppedBitmap = bitmap.Clone(cropRect, bitmap.PixelFormat))
+                        if (image.Width > image.Height)
                         {
-                            croppedBitmap.Save("IUser.jpg");
+                            NewWidth = (int)(image.Width * (256f / image.Height));
+                            NewHeight = 256;
                         }
-                    }
-                }
+                        else
+                        {
+                            NewWidth = 256;
+                            NewHeight = (int)(image.Height * (256f / image.Width));
+                        }
 
-                DoubleAnimation StartAnimation = new DoubleAnimation();
-                StartAnimation.From = 1;
-                StartAnimation.To = 0;
-                StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
-                StartAnimation.Completed += delegate
+                        // Сохраняем во временный файл
+                        string tempFile = System.IO.Path.GetTempFileName() + ".jpg";
+                        image.Save(tempFile);
+
+                        // Загружаем через System.Drawing для обрезки
+                        using (System.Drawing.Bitmap originalBitmap = new System.Drawing.Bitmap(tempFile))
+                        {
+                            // Создаем измененный размер
+                            using (System.Drawing.Bitmap resizedBitmap = new System.Drawing.Bitmap(originalBitmap, new System.Drawing.Size(NewWidth, NewHeight)))
+                            {
+                                int X = 0;
+                                int Y = 0;
+
+                                if (resizedBitmap.Width > 256)
+                                    X = (resizedBitmap.Width - 256) / 2;
+                                if (resizedBitmap.Height > 256)
+                                    Y = (resizedBitmap.Height - 256) / 2;
+
+                                // Создаем прямоугольник для обрезки
+                                System.Drawing.Rectangle cropRect = new System.Drawing.Rectangle(X, Y, 256, 256);
+
+                                // Обрезаем изображение
+                                using (System.Drawing.Bitmap croppedBitmap = resizedBitmap.Clone(cropRect, resizedBitmap.PixelFormat))
+                                {
+                                    // Сохраняем в итоговый файл
+                                    croppedBitmap.Save(outputPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                                }
+                            }
+                        }
+
+                        // Удаляем временный файл
+                        if (File.Exists(tempFile))
+                            File.Delete(tempFile);
+                    }
+
+                    // Анимация появления
+                    DoubleAnimation StartAnimation = new DoubleAnimation();
+                    StartAnimation.From = 1;
+                    StartAnimation.To = 0;
+                    StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                    StartAnimation.Completed += (s, ev) =>
+                    {
+                        if (File.Exists(outputPath))
+                        {
+                            // Загружаем изображение в WPF Image
+                            BitmapImage bitmap = new BitmapImage();
+                            bitmap.BeginInit();
+                            bitmap.UriSource = new Uri(outputPath, UriKind.Absolute);
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                            bitmap.EndInit();
+                            bitmap.Freeze();
+
+                            // User - это ваш WPF Image контрол
+                            User.Source = bitmap;
+
+                            DoubleAnimation EndAnimation = new DoubleAnimation();
+                            EndAnimation.From = 0;
+                            EndAnimation.To = 1;
+                            EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                            // Явно указываем WpfImage.OpacityProperty
+                            User.BeginAnimation(WpfImage.OpacityProperty, EndAnimation);
+                        }
+                    };
+
+                    // Явно указываем WpfImage.OpacityProperty
+                    User.BeginAnimation(WpfImage.OpacityProperty, StartAnimation);
+                    BSetImages = true;
+                }
+                catch (Exception ex)
                 {
-                    User.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"\IUser.jpg")); // Исправлено: IUser -> User, GetCurrentDirrectory -> GetCurrentDirectory
-                    DoubleAnimation EndAnimation = new DoubleAnimation();
-                    EndAnimation.From = 0;
-                    EndAnimation.To = 1;
-                    EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
-                    User.BeginAnimation(Image.OpacityProperty, EndAnimation); // Исправлено: IUser -> User
-                };
-                User.BeginAnimation(Image.OpacityProperty, StartAnimation); // Исправлено: IUser -> User
-                BSetImages = true;
+                    MessageBox.Show($"Error loading image: {ex.Message}\n\nStack trace: {ex.StackTrace}", "Image Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    BSetImages = false;
+                }
             }
             else
                 BSetImages = false;
         }
+
         private void OpenLogin(object sender, MouseButtonEventArgs e)
         {
             MainWindow.mainWindow.OpenPage(new Login());
